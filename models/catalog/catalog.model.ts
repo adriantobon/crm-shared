bba1ce6a7807602693dcff9bae5eb6ba9923f406
Adrian Tobon Guardiola <adriantobon@Adrians-MacBook-Pro.local>
2020-03-29 16:10:28 -0600

    Adding catalog model

--- models/catalog/catalog.model.ts
+++ models/catalog/catalog.model.ts
@@ -1,4 +1,19 @@
-export class CatalogResponseModel {
+class CatalogValues {
   _id: string;
   name: string;
 }
+
+class CatalogModels {
+  _id: string;
+  name: string;
+  model_values: CatalogValues[];
+}
+
+export class CatalogResponseModel {
+  _id: string;
+  name: string;
+  models: CatalogModels[];
+  isLoading?: boolean;
+  isLoaded?: boolean;
+}
+
