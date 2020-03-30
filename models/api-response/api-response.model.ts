7885099f46880b114e2f963de5002b7a3a6471ad
Adrian Tobon Guardiola <adriantobon@Adrians-MacBook-Pro.local>
2020-03-29 19:13:33 -0600

    Minor fix in api-response-model

--- models/api-response/api-response.model.ts
+++ models/api-response/api-response.model.ts
@@ -1,6 +1,6 @@
 export type ActionType = 'myAccountRequest' | 'myAccountResponse' |
                   'myAccountUpdateRequest' |
-                  'myAccountUpdatePasswordRequest'
+                  'myAccountUpdatePasswordRequest' | 'myAccountUpdatePasswordRequest'
 
 export class ApiResponseModel<data> {
   statusCode: number;
