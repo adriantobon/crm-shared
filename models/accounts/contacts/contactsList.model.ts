44ed62fa758580b655d9ee732e9293868b1e7aac
Adrian Tobon Guardiola <adriantobon@Adrians-MacBook-Pro.local>
2020-04-05 22:57:15 -0500

    Adding contactsListResponse model

---
+++ models/accounts/contacts/contactsList.model.ts
@@ -0,0 +1,10 @@
+export class ContactsListResponseModel {
+  _id: string;
+  short_name: string;
+  name: string;
+  lastname_1: string;
+  lastname_2: string;
+  position: string;
+  email: string;
+  phone: string;
+}
