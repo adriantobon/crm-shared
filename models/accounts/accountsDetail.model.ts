b437be7d8035ce6a19abe72c27e9eafdd50b11ad
Adrian Tobon Guardiola <adriantobon@Adrians-MacBook-Pro.local>
2020-04-07 01:11:47 -0500

    Adding accountsDetailResponse Model

---
+++ models/accounts/accountsDetail.model.ts
@@ -0,0 +1,59 @@
+// Se tendra que cambiar por el de detail
+import { ContactsListResponseModel } from './contacts/contactsList.model';
+import { CatalogValues } from '../catalog/catalog.model';
+
+class CreatedByModel {
+  _id?: string;
+  name?: string;
+  lastname_1?: string;
+  lastname_2?: string;
+}
+
+class AccountActivityDetails {
+  type: string;
+  account: string;
+  accountContact: string;
+}
+
+class AccountActivity {
+  details: AccountActivityDetails;
+  user: string;
+  date: Date;
+}
+
+export class AccountsDetailResponseModel {
+  _id: string;
+  autoId: number;
+  name: string;
+  social_name: string;
+  rfc: string;
+  street: string;
+  ext_number: number;
+  int_number: number;
+  colony: string;
+  zip_code: number;
+  country: string;
+  state: string;
+  city: string;
+  phones: string[];
+  short_name: string;
+  client_type: CatalogValues;
+  origin: CatalogValues;
+  business_unit: CatalogValues;
+  business_field: CatalogValues;
+  business_field_own: string;
+  aaga_front: CatalogValues;
+  client_status: CatalogValues;
+  created_by: CreatedByModel;
+  created_at: Date;
+  contacts: ContactsListResponseModel[];
+  meetings: any[];
+  status: CatalogValues;
+  activity: AccountActivity[];
+  facebook_url: string;
+  instagram_url: string;
+  linkedin_url: string;
+  linkedin_is_company_type: boolean;
+  twitter_url: string;
+  webpage_url: string;
+}
